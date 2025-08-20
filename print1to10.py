8a0259a72541fc789850fb5c854be47fbe639bec
Bellalalaa <161143292+Bellalalaa@users.noreply.github.com>
2025-08-20 16:43:45 +0400

    Add files via upload

---
+++ print1to10.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+#########1########
+
+#x = range(1,11)
+
+#for i in x:
+#	print(i)
+
+##########2#######
+
+#for i in range(1,10+1):
+#	print(i)
+
+############3########
+
+i = 1
+while i <=  10:
+	print (i)
+	i +=1
+
