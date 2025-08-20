c154742897329a482a3613260a3c8d6993e61021
Bellalalaa <161143292+Bellalalaa@users.noreply.github.com>
2025-08-20 16:23:57 +0400

    Add files via upload

---
+++ negorpos.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+num = int(input("Enter a number "))
+if num > 0:
+	print("the number is positive")
+elif num < 0 :
+	print("Your number is negative")
+else:
+	print("It's 0 ")
+
