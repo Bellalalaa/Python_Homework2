048e0974691fb44d86a743a0c40608a08a95fd39
Bellalalaa <161143292+Bellalalaa@users.noreply.github.com>
2025-08-20 16:29:44 +0400

    Add files via upload

---
+++ canvote.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+age = int(input("Please enter your age "))
+
+if age > 18 or age == 18 :
+	print("You can vote!")
+if age < 18 :
+	print("Unfortunately you wouldn't be able to vote")
+
